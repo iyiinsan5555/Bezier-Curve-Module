21648efb85443da8c018693331ed5f6a80fe0b44
iyiinsan5555 <mhbsonmez@gmail.com>
2023-10-07 13:23:46 +0300

    Edited Test Script

--- src/Scripts-ServerScriptService/Test.server.lua
+++ src/Scripts-ServerScriptService/Test.server.lua
@@ -1,4 +1,4 @@
-local BezierCurve = require(game.ReplicatedStorage:WaitForChild("Modules"):WaitForChild("iyiinsan5555's_BezierCurveModule"))
+local BezierCurveModule = require(game.ReplicatedStorage:WaitForChild("Modules"):WaitForChild("iyiinsan5555's_BezierCurveModule"))
 
 -- Define your control points as Vector3 values
 local controlPoints = {
@@ -6,18 +6,18 @@ local controlPoints = {
     Vector3.new(10,5,7.5),
     Vector3.new(-15,10,-5.5),
     Vector3.new(-30, 8, 10),
-    Vector3.new(35, 50,10)
+    Vector3.new(35, 50,10),
+    Vector3.new(50, 5, -10)
 }
 
-local bezier = BezierCurve.new(controlPoints)
+local BezierCurve = BezierCurveModule.new(controlPoints)
 
-bezier:VisualizeControlPoints()
+BezierCurve:VisualizeControlPoints()
 
 task.wait(3)
 
-for i = 0, 10, 0.1 do
-    local t = i / 10
-    local point = bezier:GetPoint(t)
+for i = 0, 1, 0.01 do
+    local point = BezierCurve:GetPoint(i)
     
     local part = Instance.new("Part")
     part.Parent = workspace
@@ -30,4 +30,4 @@ end
 
 task.wait(5)
 
-bezier:DeleteVisualazeParts()
+BezierCurve:DeleteVisualazeParts()
